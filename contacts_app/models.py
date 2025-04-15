b47ad961ce8b6d7a7bef2f688f1e73687dbad0f7
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2025-04-15 21:46:01 +0200

    feat(contacts_app): increase max_length of number field from 20 to 30

--- contacts_app/models.py
+++ contacts_app/models.py
@@ -7,7 +7,7 @@ class Contact(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True)
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    number = models.CharField(max_length=20, blank=True, null=True)
+    number = models.CharField(max_length=30, blank=True, null=True)
     first_letters = models.CharField(max_length=2, blank=True, null=True)
     is_user = models.BooleanField(default=False)
     profile_pic = models.TextField(blank=True, null=True)
